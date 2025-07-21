8c7fd3437334b61ecfebfe7f6070f43499c106c7
Ecos1213 <diasflac1213@gmail.com>
2025-07-21 12:05:45 -0500

    Explicacion de compilacion y ejecucion

--- src/main/java/Hola.java
+++ src/main/java/Hola.java
@@ -1,3 +1,6 @@
+// Se puede hacer compilacion de un archivo .java con javac
+// esta crea un archivo .class que es la compilacion para correrlo usamos el comando java
+// aunque desde las ultimas versiones de java podemos usar el comando java y el archivo .java y este compilara y ejecutara, pero este no genera el archivo .class
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
